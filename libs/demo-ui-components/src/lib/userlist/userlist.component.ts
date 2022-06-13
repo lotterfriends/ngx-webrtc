21077395235a79c17b7799bd1538b1d62492c4eb
Andre Tarnowsky <andre.tarnowsky@medondo.com>
2022-06-13 09:24:00 +0200

    fix comment

--- libs/demo-ui-components/src/lib/userlist/userlist.component.ts
+++ libs/demo-ui-components/src/lib/userlist/userlist.component.ts
@@ -57,7 +57,7 @@ export class UserlistComponent implements OnInit {
     }
   }
 
-  // Like Discord you can change local volumn mute/unmute state of participants
+  // Like in Discord you can change local volumn mute/unmute state of participants
 
   changeVolume($event: Event, user: UserInCall): void {
     const volume = parseInt(($event.target as HTMLInputElement).value, 10);
